4f2dbc1d360bd70d5ba221a530401c00b69bc354
speed lead <xuanyongyuliu@gmail.com>
2014-12-04 20:48:50 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feng::Application.config.secret_token = '41f5651dff163ccfe77f6cca489d606f9e8eb34771d8ff08cc220a3265e39ab527f34971bf141fda14781baeaa87f9f21bab5f533ba06f84ebf2b4c494477c0d'
